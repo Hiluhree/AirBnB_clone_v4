efff6baccd92df07440e58118120dcd909d164ac
Hillary Cheserek <cheserekhillary@gmail.com>
2023-11-06 06:20:41 -0800

    corrections

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -35,7 +35,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
 
     cache_id = (str(uuid.uuid4()))
-    return render_template('22-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
